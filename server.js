5ed6611abaac61286cc8168f34e7c659b1647fc0
adithyan-777 <adithyan92014@gmail.com>
2025-01-06 20:18:04 +0530

    https working

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -38,16 +40,22 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/schools', schoolRoutes);
 app.use('/api/classrooms', classroomRoutes);
-// app.use('/api/teachers', teacherRoutes);
 app.use('/api/students', studentRoutes);
 
+// Error handler middleware
 app.use(errorHandler);
 
-// Set up the server to listen on a port
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
-  logger.info(`API Docs available at http://localhost:${PORT}/api-docs`);
+// Set up SSL certificate options
+const sslOptions = {
+  cert: fs.readFileSync('/etc/ssl/certs/mydomain.crt'), // Path to your self-signed certificate
+  key: fs.readFileSync('/etc/ssl/private/mydomain.key'), // Path to your private key
+};
+
+// Set up the server to listen on a secure HTTPS port
+const PORT = process.env.PORT || 443; // Using 443 for HTTPS by default
+https.createServer(sslOptions, app).listen(PORT, () => {
+  logger.info(`HTTPS Server running on port ${PORT}`);
+  logger.info(`API Docs available at https://localhost:${PORT}/api-docs`);
 });
 
 module.exports = app;
