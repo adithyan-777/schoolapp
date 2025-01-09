e7cc38b144c1721de0bd2b9830ebae469cd090ea
adithyan-777 <adithyan92014@gmail.com>
2025-01-09 10:08:14 +0530

    updated login schema

--- schema/authSchemas.js
+++ schema/authSchemas.js
@@ -2,7 +2,7 @@ const loginSchema = {
   type: 'object',
   properties: {
     email: { type: 'string', format: 'email' },
-    password: { type: 'string', minLength: 4 },
+    password: { type: 'string', minLength: 6 },
   },
   required: ['email', 'password'],
   additionalProperties: false,
